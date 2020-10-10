5103052bd99eb78538e21a0a2009a6c10cf1a5ab
Ricky Moreno <ricky.gnr.moreno@gmail.com>
2020-10-10 14:40:40 -0300

    ejemplo sencillo TODO list

---
+++ modulo2/DOM/script.js
@@ -0,0 +1,17 @@
+const tareas = []
+
+const form = document.getElementById('form-tareas')
+
+form.onsubmit= (e) => {
+    e.preventDefault()
+    const tarea = document.getElementById('tarea')
+    const tareaText = tarea.value
+    tarea.value= ''
+    console.log(tareaText);
+    tareas.push(tareaText)
+    const listaTareas = document.getElementById('lista-tareas')
+    listaTareas.innerHTML = ''
+    for (let i = 0; i < tareas.length; i++) {
+        listaTareas.innerHTML += '<li class="list-group-item">' + tareas[i] + '</li>'
+    }
+}
