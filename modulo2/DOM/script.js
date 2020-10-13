f25fd637bdc890dcf7b326e56a8596f89c8d0ea3
JJFAO <jjfalonso@gmail.com>
2020-10-13 09:23:16 -0300

    Practica DOM: refactoreo y comentarios

--- modulo2/DOM/script.js
+++ modulo2/DOM/script.js
@@ -1,20 +1,34 @@
-const tareas = []
+const tareas = [];
 
-const form = document.getElementById('form-tareas')
+const form = document.getElementById('formTareas');
 
-form.onsubmit= (e) => {
-    e.preventDefault()
-    const tarea = document.getElementById('tarea')
-    const tareaText = tarea.value
-    tarea.value= ''
-    console.log(tareaText);
-    tareas.push(tareaText)
-    const listaTareas = document.getElementById('lista-tareas')
-    listaTareas.innerHTML = ''
-    for (let i = 0; i < tareas.length; i++) {
-        listaTareas.innerHTML += '<li class="list-group-item">' + tareas[i] + '</li>'
-    }
-
-    // const listaTempalte = tareas.map(t => '<li class="list-group-item">' + t + '</li>')
-    // listaTareas.innerHTML = listaTempalte.join('')
+function listarTarea(tarea, indice) {
+    // return '<li class="list-group-item">' + (indice + 1) + ' - ' + tarea + '</li>';
+    return `<li class="list-group-item">${indice + 1} - ${tarea}</li>`;
 }
+
+form.onsubmit = function (e) {
+    e.preventDefault();
+    const listaTareasHtml = document.getElementById('listaTareas');
+    const tareaHtml = document.getElementById('tarea');
+    const tareaText = tareaHtml.value;
+    tareas.push(tareaText);
+
+    // const tareasLi = [];
+    // for (let i = 0; i < tareas.length; i++) {
+    //     const tareaLi = listarTarea(tareas[i], i);
+    //     tareasLi.push(tareaLi);
+    // }
+    // listaTareasHtml.innerHTML = tareasLi.join('');
+    // -- Este tipo de bucles sobre un array son habituales,
+    // por eso existen varios métodos de arrays que nos simplifican esta tarea👇. --
+
+    listaTareasHtml.innerHTML = tareas.map(listarTarea).join('');
+    // -- Si el retorno de una función o método es un objeto,
+    // podemos acceder a sus propiedades en la misma linea. --
+
+    // const listaTemplate = tareas.map(t => '<li class="list-group-item">' + t + '</li>')
+    // listaTareasHtml.innerHTML = listaTemplate.join('')
+
+    tareaHtml.value = '';
+};
