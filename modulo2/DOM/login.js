fb2055517842ed550f58bc8f86b68330f77cfe24
JJFAO <jjfalonso@gmail.com>
2020-12-02 04:05:38 -0300

    Login LocalStorage: guardar el nombre del usuario logueado.

--- modulo2/DOM/login.js
+++ modulo2/DOM/login.js
@@ -3,18 +3,30 @@ const inputUsuario = document.getElementById('usuario');
 const inputContrasenia = document.getElementById('contrasenia');
 const alerta = document.getElementById('alerta');
 
-const persona = {
-    usuario: 'jj',
-    contrasenia: 'asd',
-};
+const usuarios = [
+    {
+        apodo: 'jj',
+        contrasenia: 'asd',
+    },
+    {
+        apodo: 'jj2',
+        contrasenia: 'asd2',
+    }
+];
 
 formLogin.onsubmit = function (e) {
     e.preventDefault();
-    const usuario = inputUsuario.value;
+    const apodo = inputUsuario.value;
     const contrasenia = inputContrasenia.value;
-    const usuarioValido = usuario === persona.usuario &&
-        contrasenia === persona.contrasenia;
-    if (usuarioValido) {
+    const usuarioEncontrado = usuarios.find((usuario) => {
+        // Esta función callback declarada en el método find() retorna false,
+        // hasta encontrar un usuario que coincida con los datos ingresados.
+        // Al cumplirse la condición retorna true, 
+        return apodo === usuario.apodo && contrasenia === usuario.contrasenia;
+    });
+    if (usuarioEncontrado) {
+
+        localStorage.setItem('userLogged', usuarioEncontrado.apodo);
         alert('Logueo Exitoso');
         window.location.href = 'index.html';
     } else {
