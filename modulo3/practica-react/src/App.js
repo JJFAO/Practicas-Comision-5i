c99c970f8ab7851ad5c947602e6697a07145e684
JJFAO <jjfalonso@gmail.com>
2020-12-10 19:54:04 -0300

    React: formulario y renderizado condicional.

--- modulo3/practica-react/src/App.js
+++ modulo3/practica-react/src/App.js
@@ -3,42 +3,47 @@ import logo from './logo.svg';
 import './App.scss';
 import Header from './Header';
 import Main from './Main';
-import { useState } from 'react';
+import {useState} from 'react';
 import Personas from './Personas';
+import Form from './Form';
 
 function App() {
-  const info = {
-    title: 'Mi título',
-    description: 'Some quick example text to build on the card '
-  };
+    const [section, setSection] = useState('principal');
+    console.log('App - section', section);
+    const info = {
+        title: 'Mi título',
+        description: 'Some quick example text to build on the card ',
+    };
 
-  return (
-    <div className="App bg-fondo">
-      {/*Al componente Header le enviamos la url de la imagen */}
-      <Header logo={logo} />
-      {/*Al componente Main le enviamos el objeto info */}
-      <Main data={info} />
-      <Example />
-      <Personas />
-    </div>
-  );
+    return (
+        <div className="App bg-fondo">
+            {/*Al componente Header le enviamos la url de la imagen */}
+            <Header logo={logo} changeSection={setSection} />
+            {/*Al componente Main le enviamos el objeto info */}
+            {section === 'principal' && (
+                <div>
+                    <Main data={info} />
+                    <Example />
+                    <Personas />
+                </div>
+            )}
+            {section === 'formulario' && <Form />}
+        </div>
+    );
 }
 
-
 function Example() {
-  // Declaración de una variable de estado que llamaremos "count"
-  const [count, setCount] = useState(0);
-  
-  return (
-    <div>
-      {/* En este <p> se muestra el valor actual del state 'count'. */}
-      <p>You clicked {count} times</p>
-      {/* De esta forma asignamos una función o callback para que se ejecute en el evento click del botón */}
-      <button onClick={() => setCount(count + 1)}>
-        Click me
-      </button>
-    </div>
-  );
+    // Declaración de una variable de estado que llamaremos "count"
+    const [count, setCount] = useState(0);
+
+    return (
+        <div>
+            {/* En este <p> se muestra el valor actual del state 'count'. */}
+            <p>You clicked {count} times</p>
+            {/* De esta forma asignamos una función o callback para que se ejecute en el evento click del botón */}
+            <button onClick={() => setCount(count + 1)}>Click me</button>
+        </div>
+    );
 }
 
 export default App;
--- modulo3/practica-react/src/Header.js
+++ modulo3/practica-react/src/Header.js
@@ -8,16 +8,16 @@ import Button from 'react-bootstrap/Button';
 // Al igual que con los componentes que creamos,
 // los componentes de librerías también se deben importar antes de usarlos.
 
-export default function Header() {
+export default function Header(props) {
     return (
         <header className="">
-            <Navbar bg="light" expand="lg">
+            <Navbar bg="dark" variant="dark" expand="lg">
                 <Navbar.Brand href="#home">Práctica React</Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="mr-auto">
-                        <Nav.Link href="#home">Home</Nav.Link>
-                        <Nav.Link href="#link">Link</Nav.Link>
+                        <Nav.Link onClick={() => props.changeSection('principal')} >Home</Nav.Link>
+                        <Nav.Link onClick={() => props.changeSection('formulario')} >Formulario</Nav.Link>
                         <NavDropdown title="Dropdown" id="basic-nav-dropdown">
                             <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
                             <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
@@ -28,7 +28,7 @@ export default function Header() {
                     </Nav>
                     <Form inline>
                         <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-                        <Button variant="outline-success">Search</Button>
+                        <Button variant="danger">Search</Button>
                     </Form>
                 </Navbar.Collapse>
             </Navbar>
