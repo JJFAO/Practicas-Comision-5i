cc5c88ae41600d323129356517f2e115913421be
JJFAO <jjfalonso@gmail.com>
2020-12-10 21:31:28 -0300

    React: carga de articulos con formulario, states y localstorage.

--- modulo3/practica-react/src/Form.js
+++ modulo3/practica-react/src/Form.js
@@ -1,82 +1,53 @@
-import { useState } from 'react'
+import {useState} from 'react';
 import { Form, InputGroup, Button, Col } from 'react-bootstrap';
+import { getStorageArray, setStorage } from './utils';
+
+
 
 export default function TheForm() {
     const [validated, setValidated] = useState(false);
+    const [input, setInput] = useState({ title: '', image: '', description: '' });
+    // const [articles, setArticles] = useState([]);
 
     const handleSubmit = (event) => {
-        const form = event.currentTarget;
-        if (form.checkValidity() === false) {
-            event.preventDefault();
-            event.stopPropagation();
-        }
-
+        const form = event.target;
+        event.preventDefault();
+        event.stopPropagation();
         setValidated(true);
+        if (form.checkValidity() === true) {
+            const articles = getStorageArray('articles');
+            const updatedArticles = [...articles, input];
+            setStorage('articles', updatedArticles);
+            // setArticles(updatedArticles);
+        }
+    };
+
+    const handleChange = (e) => {
+        const value = e.target.value;
+        const name = e.target['name'];
+        const obj = { ...input, [name]: value };
+        setInput(obj);
     };
 
     return (
-        <div>
+        <div style={{width: '500px', margin: 'auto'}} className="mt-5">
             <Form noValidate validated={validated} onSubmit={handleSubmit}>
-                <Form.Row>
-                    <Form.Group as={Col} md="4" controlId="validationCustom01">
-                        <Form.Label>First name</Form.Label>
-                        <Form.Control required type="text" placeholder="First name" defaultValue="Mark" />
-                        <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
-                    </Form.Group>
-                    <Form.Group as={Col} md="4" controlId="validationCustom02">
-                        <Form.Label>Last name</Form.Label>
-                        <Form.Control required type="text" placeholder="Last name" defaultValue="Otto" />
-                        <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
-                    </Form.Group>
-                    <Form.Group as={Col} md="4" controlId="validationCustomUsername">
-                        <Form.Label>Username</Form.Label>
-                        <InputGroup>
-                            <InputGroup.Prepend>
-                                <InputGroup.Text id="inputGroupPrepend">@</InputGroup.Text>
-                            </InputGroup.Prepend>
-                            <Form.Control
-                                type="text"
-                                placeholder="Username"
-                                aria-describedby="inputGroupPrepend"
-                                required
-                            />
-                            <Form.Control.Feedback type="invalid">
-                                Please choose a username.
-                            </Form.Control.Feedback>
-                        </InputGroup>
-                    </Form.Group>
-                </Form.Row>
-                <Form.Row>
-                    <Form.Group as={Col} md="6" controlId="validationCustom03">
-                        <Form.Label>City</Form.Label>
-                        <Form.Control type="text" placeholder="City" required />
-                        <Form.Control.Feedback type="invalid">
-                            Please provide a valid city.
-                        </Form.Control.Feedback>
-                    </Form.Group>
-                    <Form.Group as={Col} md="3" controlId="validationCustom04">
-                        <Form.Label>State</Form.Label>
-                        <Form.Control type="text" placeholder="State" required />
-                        <Form.Control.Feedback type="invalid">
-                            Please provide a valid state.
-                        </Form.Control.Feedback>
-                    </Form.Group>
-                    <Form.Group as={Col} md="3" controlId="validationCustom05">
-                        <Form.Label>Zip</Form.Label>
-                        <Form.Control type="text" placeholder="Zip" required />
-                        <Form.Control.Feedback type="invalid">
-                            Please provide a valid zip.
-                        </Form.Control.Feedback>
-                    </Form.Group>
-                </Form.Row>
-                <Form.Group>
-                    <Form.Check
-                        required
-                        label="Agree to terms and conditions"
-                        feedback="You must agree before submitting."
-                    />
+                <Form.Group controlId="title">
+                    <Form.Label>Título</Form.Label>
+                    <Form.Control name="title" required type="text" onChange={handleChange} />
+                    <Form.Control.Feedback>se ve bien!</Form.Control.Feedback>
+                    <Form.Control.Feedback type="invalid">no se ve bien!</Form.Control.Feedback>
                 </Form.Group>
-                <Button type="submit">Submit form</Button>
+                <Form.Group controlId="image">
+                    <Form.Label>Imagen</Form.Label>
+                    <Form.Control name="image" required type="text" onChange={handleChange} />
+                    <Form.Control.Feedback>se ve bien!</Form.Control.Feedback>
+                </Form.Group>
+                <Form.Group controlId="description">
+                    <Form.Label>Descripción</Form.Label>
+                    <Form.Control name="description" as="textarea" rows={3} onChange={handleChange} />
+                </Form.Group>
+                <Button type="submit">Cargar artículo</Button>
             </Form>
         </div>
     );
